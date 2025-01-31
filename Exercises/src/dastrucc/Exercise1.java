a1781b99ec76d93d1e8974101f7ad1d197649d86
Ninoy <krisslantuan0@gmail.com>
2025-01-30 23:14:24 -0800

    added Exercise 1

---
+++ Exercises/src/dastrucc/Exercise1.java
@@ -0,0 +1,11 @@
+package dastrucc;
+
+class Exercise1 {
+	public static void main(String[] args) {
+		double[] myArray = {1,2,3,4,5,6,7,8,9,10};
+		for (int x = 0; x < myArray.length; x++) {
+			System.out.println(myArray[x] + "");
+		
+		}
+	}
+}
