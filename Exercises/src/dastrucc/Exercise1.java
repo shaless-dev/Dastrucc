85040eed43c867548f919fbe404714a964e0f1eb
Ninoy <krisslantuyan0@gmail.com>
2025-02-03 16:43:30 -0800

    updated exercise1 code

--- Exercises/src/dastrucc/Exercise1.java
+++ Exercises/src/dastrucc/Exercise1.java
@@ -2,10 +2,63 @@ package dastrucc;
 
 class Exercise1 {
 	public static void main(String[] args) {
-		double[] myArray = {1,2,3,4,5,6,7,8,9,10};
+		int[] myArray = {1,2,3,4,5,6,8,7,9,10};
 		for (int x = 0; x < myArray.length; x++) {
-			System.out.println(myArray[x] + "");
-		
-		}
+			}
+			//largest value
+			int max = myArray[0];
+			for (int x= 0; x < myArray.length; x++) {
+				if (myArray[x] > max) max = myArray[x];
+			}
+			System.out.println("Largest is " + max);
+			//smallest value
+			int min = myArray[0];
+			
+			for (int x= 0; x > myArray.length; x++) {
+				if (myArray[x] > min) min = myArray[x];
+			}
+			System.out.println("Smallest is " + min);
+			//sum of all array elements
+			
+			int total = 0;
+			for(int x = 0; x < myArray.length; x++) {
+				total += myArray[x];
+			}
+			System.out.println("Total is " + total);
+			//ascending order
+			System.out.println("Ascending:");
+			for (int x = 0; x < myArray.length - 1; x++) {
+	           for (int j = x + 1; j < myArray.length; j++){
+	        	   if(myArray[x] > myArray[j]) {
+	        		   int temp = myArray[x];
+	        		   myArray[x] = myArray[j];
+	        		   myArray[j] = temp;
+	        	   }
+	           }
+			}
+			
+			for(int i = 0;i < myArray.length;i++) {
+				System.out.print("[" + myArray[i] + "]");
+			}
+			//descending order
+			System.out.println(" "); 
+			System.out.println("Descending:");
+			for (int x = 0; x < myArray.length - 1; x++) {
+		           for (int j = x + 1; j < myArray.length; j++){
+		        	   if(myArray[x] < myArray[j]) {
+		        		   int temp = myArray[x];
+		        		   myArray[x] = myArray[j];
+		        		   myArray[j] = temp;
+		        	   }
+		           }
+				}
+				
+				for(int i = 0;i < myArray.length;i++) {
+					System.out.print("[" + myArray[i] + "]");
+				}
+			//average of array elements
+	        System.out.println("");
+	            double average = (double) total / myArray.length; 
+	            System.out.println("Average is " + average);
+			}
 	}
-}
